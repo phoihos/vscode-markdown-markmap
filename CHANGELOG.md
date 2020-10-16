af30b4bb1140648fdf60b2f6f8a7911b2ba1f618
Jay Park <phoihos@gmail.com>
2020-10-16 15:54:50 +0900

    Fix a issue
    - Fixed : Not working with `{key=value}` attributes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the "Markdown Preview Markmap Support" extension will be documented in this file.
 
+## [1.0.1] - 2020-10-16
+
+- Fixed : Not working with `{key=value}` attributes
+
 ## [1.0.0] - 2020-10-14
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import markdownItMarkmap from './markdownItMarkmap'
 import MarkdownIt = require('markdown-it');
+const markdownItAttrs = require('markdown-it-attrs');
+import markdownItMarkmap from './markdownItMarkmap'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate() {
 	return {
 		extendMarkdownIt(md: MarkdownIt) {
-			return md.use(markdownItMarkmap);
+			return md.use(markdownItAttrs).use(markdownItMarkmap);
 		}
 	};
 }
